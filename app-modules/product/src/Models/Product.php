e49a010c07e4424de33842e89559fcfdc3f3f763
Ehsan Mahmoodi <misaf.1990@gmail.com>
2024-07-27 03:08:50 +0330

    update

--- app-modules/product/src/Models/Product.php
+++ app-modules/product/src/Models/Product.php
@@ -86,6 +86,6 @@ final class Product extends BaseModelWithMedia implements
 
     protected static function booted(): void
     {
-        static::creating(fn(self $product): int => $product->token = fake()->randomNumber(9, true));
+        static::creating(fn(self $product): string => $product->token = substr(str_shuffle(str_repeat('123456789', 9)), 0, 9));
     }
 }
