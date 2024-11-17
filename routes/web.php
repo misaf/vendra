b0702b6e0ef28a2355a3c88773137febc0118ae1
Ehsan Mahmoodi <misaf.1990@gmail.com>
2024-11-17 10:14:59 +0330

    update

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,4 @@ declare(strict_types=1);
 use Illuminate\Support\Facades\Route;
 use Livewire\Livewire;
 
-Livewire::setUpdateRoute(fn($handle) => Route::post('/admin/livewire/update', $handle));
-
 // Route::get('xxx', fn() => 'as')->name('xxx.index');
