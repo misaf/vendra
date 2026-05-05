978c9f1e6ce3e718a3f247552517bdd90ae02f3d
Misaf <misaf.1990@gmail.com>
2026-05-05 14:17:31 +0330

    Updated config files

---
+++ config/money.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    /*
+     |--------------------------------------------------------------------------
+     | Laravel money
+     |--------------------------------------------------------------------------
+     */
+    'locale'            => config('app.locale', 'en_US'),
+    'defaultCurrency'   => config('app.currency', 'USD'),
+    'defaultFormatter'  => null,
+    'defaultSerializer' => null,
+    'isoCurrenciesPath' => is_dir(__DIR__ . '/../vendor')
+        ? __DIR__ . '/../vendor/moneyphp/money/resources/currency.php'
+        : __DIR__ . '/../../../moneyphp/money/resources/currency.php',
+    'currencies' => [
+        'iso'     => '',
+        'bitcoin' => 'all',
+        'custom'  => [
+            'IRR' => 0,
+        ],
+    ],
+];
