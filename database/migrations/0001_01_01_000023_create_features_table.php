a13e236799344071883196277a720202cdfdbea2
Misaf <misaf.1990@gmail.com>
2026-06-09 14:08:28 +0330

    feat: add features table migration

---
+++ database/migrations/0001_01_01_000023_create_features_table.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Laravel\Pennant\Migrations\PennantMigration;
+
+return new class () extends PennantMigration {
+    public function up(): void
+    {
+        Schema::create('features', function (Blueprint $table): void {
+            $table->id();
+            $table->string('name');
+            $table->string('scope');
+            $table->text('value');
+            $table->timestamps();
+
+            $table->unique(['name', 'scope']);
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('features');
+    }
+};
