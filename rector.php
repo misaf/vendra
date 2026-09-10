2b754063a1482ed31895920a377bb232fafb9439
Misaf <misaf.1990@gmail.com>
2026-09-10 19:29:01 +0330

    chore(rector): expand rule set with Laravel modernization rules
    Add ArrayToArrGetRector, AddHasFactoryToModelsRector,
    ApplyDefaultInsteadOfNullCoalesceRector, ConfigToTypedConfigMethodCallRector,
    FactoryFuncCallToStaticCallRector, AbortIfRector, ThrowIfRector,
    ReportIfRector, DateWhereClauseToShorthandRector,
    EloquentMagicMethodToQueryBuilderRector, EloquentOrderByToLatestOrOldestRector,
    FactoryApplyingStatesRector, OptionalToNullsafeOperatorRector,
    RequestStaticValidateToInjectRector, and WhereNullComparisonToWhereNullRector.
    
    Remove LARAVEL_STATIC_TO_INJECTION (covered by individual rules) and add
    the Pest coding style set for consistent formatting.

--- rector.php
+++ rector.php
@@ -4,11 +4,26 @@ declare(strict_types=1);
 
 use Pest\Rector\Set\PestSetList;
 use Rector\Config\RectorConfig;
+use RectorLaravel\Rector\ArrayDimFetch\ArrayToArrGetRector;
+use RectorLaravel\Rector\Class_\AddHasFactoryToModelsRector;
 use RectorLaravel\Rector\Class_\RemoveModelPropertyFromFactoriesRector;
 use RectorLaravel\Rector\Class_\UseForwardsCallsTraitRector;
+use RectorLaravel\Rector\Coalesce\ApplyDefaultInsteadOfNullCoalesceRector;
 use RectorLaravel\Rector\Empty_\EmptyToBlankAndFilledFuncRector;
+use RectorLaravel\Rector\FuncCall\ConfigToTypedConfigMethodCallRector;
+use RectorLaravel\Rector\FuncCall\FactoryFuncCallToStaticCallRector;
+use RectorLaravel\Rector\If_\AbortIfRector;
+use RectorLaravel\Rector\If_\ReportIfRector;
+use RectorLaravel\Rector\If_\ThrowIfRector;
+use RectorLaravel\Rector\MethodCall\DateWhereClauseToShorthandRector;
+use RectorLaravel\Rector\MethodCall\EloquentOrderByToLatestOrOldestRector;
+use RectorLaravel\Rector\MethodCall\FactoryApplyingStatesRector;
 use RectorLaravel\Rector\MethodCall\ResponseHelperCallToJsonResponseRector;
+use RectorLaravel\Rector\MethodCall\WhereNullComparisonToWhereNullRector;
+use RectorLaravel\Rector\PropertyFetch\OptionalToNullsafeOperatorRector;
+use RectorLaravel\Rector\StaticCall\EloquentMagicMethodToQueryBuilderRector;
 use RectorLaravel\Rector\StaticCall\MinutesToSecondsInCacheRector;
+use RectorLaravel\Rector\StaticCall\RequestStaticValidateToInjectRector;
 use RectorLaravel\Set\LaravelSetList;
 
 return RectorConfig::configure()
@@ -35,17 +50,31 @@ return RectorConfig::configure()
         LaravelSetList::LARAVEL_COLLECTION,
         LaravelSetList::LARAVEL_CONTAINER_STRING_TO_FULLY_QUALIFIED_NAME,
         LaravelSetList::LARAVEL_FACTORIES,
-        LaravelSetList::LARAVEL_STATIC_TO_INJECTION,
         LaravelSetList::LARAVEL_TESTING,
         LaravelSetList::LARAVEL_TYPE_DECLARATIONS,
         PestSetList::CODING_STYLE,
     ])
     ->withRules([
+        AbortIfRector::class,
+        AddHasFactoryToModelsRector::class,
+        ApplyDefaultInsteadOfNullCoalesceRector::class,
+        ArrayToArrGetRector::class,
+        ConfigToTypedConfigMethodCallRector::class,
+        DateWhereClauseToShorthandRector::class,
+        EloquentMagicMethodToQueryBuilderRector::class,
+        EloquentOrderByToLatestOrOldestRector::class,
         EmptyToBlankAndFilledFuncRector::class,
+        FactoryApplyingStatesRector::class,
+        FactoryFuncCallToStaticCallRector::class,
         MinutesToSecondsInCacheRector::class,
+        OptionalToNullsafeOperatorRector::class,
         RemoveModelPropertyFromFactoriesRector::class,
+        ReportIfRector::class,
+        RequestStaticValidateToInjectRector::class,
         ResponseHelperCallToJsonResponseRector::class,
+        ThrowIfRector::class,
         UseForwardsCallsTraitRector::class,
+        WhereNullComparisonToWhereNullRector::class,
     ])
     ->withImportNames(
         removeUnusedImports: true,
